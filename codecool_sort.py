13ba6b90d4c0c11d1ceeb22773e3b40c8c159761
AJuszczakiewicz <al.jusz@gmail.com>
2017-12-27 19:26:37 +0200

    Add tests to codecool_sort

--- codecool_sort.py
+++ codecool_sort.py
@@ -1,11 +1,16 @@
-numbers = input("Gimme some numbers to sort: ").split()
-print(numbers)
-iterations = 1
-N = len(numbers)
-while iterations < N:
-    j = 0
-    while j <= N:
-        if numbers[j] > numbers[j+1]:
-            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-        j = j+1
-    iterations = iterations + 1
+# num_list = input("Gimme some num_list to sort: ").split()
+# print(num_list)
+num_list = [3,2,6,1,67,3,5,1]
+
+def codecool_sort(num_list):
+    N = len(num_list)
+    for x in range(0, N-1):
+        for j in range(0, N-1):
+            if num_list[j] > num_list[j+1]:
+                num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
+    return num_list
+    
+def test_codecool_sort():
+    assert codecool_sort([3,2,6,1,67,3,5,1]) == [1, 1, 2, 3, 3, 5, 6, 67]
+    assert codecool_sort([2,1]) == [1,2]
+    assert codecool_sort([]) == []
