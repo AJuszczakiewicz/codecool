aa1ace33710f18b8dc1090d60687599e8d9824d4
AJuszczakiewicz <al.jusz@gmail.com>
2018-01-16 09:47:00 +0100

    delate moved files

--- simple_calculator.py
+++
@@ -1,9 +0,0 @@
-import operator
-
-num1 = input("Enter a number (or a letter to exit): ")
-while num1.isdigit():
-    ops = { "+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv, "**": operator.pow, "%": operator.mod }
-    op = input("Enter an operation: ")
-    num2 = input("Enter another number: ")
-    print("Result: " + str(ops[op](float(num1), float(num2))))
-    num1 = input("Enter a number (or a letter to exit): ")
